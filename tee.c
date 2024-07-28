95927370979cd95ec43a741bfb86dbcfc274ff90
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-07-28 21:38:43 +0800

    fix: stdout handler

--- tee.c
+++ tee.c
@@ -463,8 +463,8 @@ int wmain(const int argc, const wchar_t *const argv[])
 
     /* Enable ANSI escape code processing of stdout */
     const DWORD stdOutMode = 0;
-    GetConsoleMode(hConsole, &stdOutMode);
-    SetConsoleMode(hConsole, stdOutMode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    GetConsoleMode(hStdOut, &stdOutMode);
+    SetConsoleMode(hStdOut, stdOutMode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 
     /* Initialize read/write locks and condition variables */
     for (DWORD index = 0; index < BUFFERS; ++index)
